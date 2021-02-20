72706521c77a205da4814d7b9b036eb6a9e5bd7e
GUNER Yusuf <yusuf_guner@msn.com>
2021-02-20 23:29:13 +0100

    Feat: Template formulaire connexion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FormConnexionComponent } from './form-connexion/form-connexion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormConnexionComponent
   ],
   imports: [
     BrowserModule,
