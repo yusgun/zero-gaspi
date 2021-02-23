a277e36d7ca334c175115ba30e56d5abbed7cdc0
bilelboukherbache <bilel.boukherbache@outlook.fr>
2021-02-23 13:56:21 +0100

    navbar du vendeur, sans image

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarVendeurComponent } from './navbar-vendeur/navbar-vendeur.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    NavbarVendeurComponent
   ],
   imports: [
     BrowserModule,
