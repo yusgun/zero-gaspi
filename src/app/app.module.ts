b87fa7f917fb4fa508e95a773ffa5c14ff7340f6
alexandrewolny <alexandrewolny>
2021-02-25 22:56:47 +0100

    fix: Ajout Jquery pour menu deroulant

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NavbarComponent } from './navbar/navbar.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
