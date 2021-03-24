9e6f010509e42e609a7a5b655feca00e315d7fe2
Yusuf GUNER <yusuf_guner@msn.com>
2021-03-24 09:59:13 +0100

    Ajout de la premiere route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageAcheteurComponent } from './page-acheteur/page-acheteur.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"acheteur", component: PageAcheteurComponent, pathMatch: "full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
